6c32543013baf48d49526acb38d306d6c783d07f
Alisher Aliev <alisher.aliev@aalto.fi>
2022-10-14 20:48:44 +0300

    Configure colors; Add ARA logos; Make initial navigation header design; Remove auto-generated CSS rules in App.css;

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
 import './App.css';
+import logo from './logo.jpg';
 
 const App = () => {
   return(
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-    </h1>
+    <div className='h-screen max-w-5xl mx-auto'>
+      <header id='navigation-bar' className='flex items-center justify-between p-6'>
+        <div id='logo-home' className='flex justify-start'>
+          <a href="#" className='h-8 w-8 sm:h-16 sm:w-16 overflow-hidden rounded-full '>
+            <span className="sr-only">Your Company</span>
+            <img className="hover:scale-150" src={logo}></img>
+          </a>
+        </div>
+        <nav id='navigation-tabs' className='flex space-x-10'>
+          <a id='home-tab' href="#" className="text-base font-medium text-black hover:text-blue">Home</a>
+          <a id='events-tab' href="#" className="text-base font-medium text-black hover:text-blue">Events</a>
+          <a id='board-tab' href="#" className="text-base font-medium text-black hover:text-blue">Board</a>
+          <a id='membership-tab' href="#" className="text-base font-medium text-black hover:text-blue">Membership</a>
+          <a id='contacts-tab' href="#" className="text-base font-medium text-black hover:text-blue">Contacts</a>
+        </nav>
+        <div id='language-setter' className='flex justify-end overflow-hidden'>
+          <button className='inline-flex items-center justify-center px-4 py-2 bg-blue text-base font-semibold rounded-md text-white hover:bg-green'>EN</button>
+        </div>
+      </header>
+    </div>
   );
 };
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{ts,tsx}",
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {},
+    colors: {
+      'red': '#ef303f',
+      'blue': '#3390ec',
+      'black': '#0c090d',
+      'green': '#06ba63',
+      'yellow': 'f9c22e',
+      'white': '#ffffff'
+    },
   },
   plugins: [],
-}
+};
