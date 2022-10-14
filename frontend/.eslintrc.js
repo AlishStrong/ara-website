9ee0fb8d75c7a14bcaa2cda9dcdbe4cdb2c5a70d
Alisher Aliev <alisher.aliev@aalto.fi>
2022-10-14 09:52:12 +0300

    Configure eslint;

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'settings': {
+    'react': {
+      'pragma': 'React',
+      'version': 'detect'
+    }
+  },
+  'rules': {
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/react-in-jsx-scope': 0, // suppress errors for missing 'import React' in files
+    'indent': [
+      'error',
+      2,
+      { 'SwitchCase': 1 }
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'eol-last': [
+      'error', 'always'
+    ],
+  }
+};
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,8 @@ const App = () => {
   return(
     <h1 className="text-3xl font-bold underline">
     Hello world!
-  </h1>
-  )
-}
+    </h1>
+  );
+};
 
 export default App;
